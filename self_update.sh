50e4dd52c9bb60d287f8099c39fd2f6e57868d0c
Brian Cummings <brian.m.cummings@gmail.com>
2017-07-27 08:19:41 -0400

    Added self_update

---
+++ self_update.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script
+
+cd /
+cd home/pi/python/temp_sensor
+# Wait for network to be up (pinging Google)
+while ! ping -c 1 -W 1 8.8.8.8; do
+    sleep 1
+done
+sudo pkill python
+sudo apt-get update
+sudo apt-get -y dist-upgrade
+sudo reboot
